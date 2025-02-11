e02b9a00890c8ef6807770e8654347fe787acc7b
Fossi5 <fossijoel5@gmail.com>
2025-02-11 16:53:14 +0100

    commit1

---
+++ src/main/java/com/badier/badier_ride/BadierRideApplication.java
@@ -0,0 +1,13 @@
+package com.badier.badier_ride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadierRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadierRideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/badier/badier_ride/BadierRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badier.badier_ride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadierRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
